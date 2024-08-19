1392c8da39e603b15d63b61be2932a3cac9a9724
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2024-08-19 20:26:11 -0300

    Atividade 01 de VueJS

---
+++ script.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#saudacao',
+    data: {
+    greeting: 'Olá, Carlos Vechi!',
+    mensagem: 'Seja bem bem vindo novamente! Estávamos ansiosos pela sua volta. Sentimentos sua falta! Último login foi há: 3 anos, 3 meses, 3 dias e 33 horas :('
+    }
+}) 
+
+var app = new Vue({
+    el: '#top',
+    data: {
+    enois: 'UniSenai',
+    tico: 'Primeira atividade usando VueJS + HTML e CSS'
+    }
+}) 
