bc860d1292c8a8f80cba5d1e449bd241526cab6b
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2024-08-20 21:16:52 -0300

    v-if, v-else, v-show
    Neste exemplo, o parágrafo com v-if será renderizado se a propriedade seen for
    verdadeira. Caso contrário, o parágrafo com v-else será renderizado.

---
+++ scriptz.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        usuario: { 
+            id: 1,
+            nome: 'João',
+            email: 'joaosilva.com',
+            profissao: 'Garoto de programa'
+
+            // essas informações acima, são as PROPRIEDADES que compoem o objeto 'usuario'.
+        }
+    }
+})
