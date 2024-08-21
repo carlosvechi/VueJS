45668a583ff84617965fb56d312ec37c6e49a037
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2024-08-20 21:50:10 -0300

    v-for

---
+++ scriipt.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Aprendendo Vue JS',
+        linguagens: [
+            {nome: 'HTML', status: true},
+            {nome: 'CSS', status: true},
+            {nome: 'JavaScript', status: false},
+            {nome: 'PHP', status: false},
+            {nome: 'Python', status: false},
+        ]
+    }
+})
