2557eeb8945aa3eab5611b05106a687ac2b18f23
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2024-08-22 20:01:01 -0300

    validação de login estilizado

--- scrript.js
+++ scrript.js
@@ -1,29 +1,33 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        titulo: 'Aplicação de Login',
-        autenticado: false,
-        usuario: {
-            nome: 'João'
-        },
-        username: '',
-        password: '',
-        erro: false
-    },
-    methods: {
-        login() {
-            //Simulação de login
-            if (this.username === 'usuario' && this.password === 'senha') {
-                this.autenticado = true;
-                this.erro = false;
-            } else {
-                this.erro = true;
+new Vue({
+            el: '#app',
+            data: {
+                titulo: 'Tela de Login',
+                autenticado: false,
+                usuario: { nome: 'João' },
+                username: '',
+                password: '',
+                erro: false,
+                mostrarDialogo: false,
+            },
+            methods: {
+                login() {
+                    // Simulação de login com credenciais fixas
+                    if (this.username === 'user' && this.password === 'password') {
+                        this.autenticado = true;
+                        this.erro = false;
+                        this.mostrarDialogo = false;
+                    } else {
+                        this.erro = true;
+                        this.mostrarDialogo = true;
+                    }
+                },
+                logout() {
+                    this.autenticado = false;
+                    this.username = '';
+                    this.password = '';
+                },
+                fecharDialogo() {
+                    this.mostrarDialogo = false;
+                }
             }
-        },
-        logout() {
-            this.autenticado = false;
-            this.username = '';
-            this.password = '';
-        }
-    }
-})
+        });
