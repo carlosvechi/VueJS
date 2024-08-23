bb6644be5c6cd30b3f5bd089d376340b56edcb01
Carlos Vechi <161260318+carlosvechi@users.noreply.github.com>
2024-08-22 21:51:12 -0300

    Formulário

---
+++ sccript.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Aprendendo Vue JS',
+        nome: "",
+        mail: "",
+        novidades: "",
+        interesses: [],
+        conheceu: ""
+        
+    }
+})
